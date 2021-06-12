80da060371de47ccc1d85387d921ac359c09ee4f
Liam <liam.s.oconnell@gmail.com>
2021-06-12 10:00:38 +1000

    Setup audio buses and global variables

--- GMTKJAM2021/script/Master.gd
+++ GMTKJAM2021/script/Master.gd
@@ -8,7 +8,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Global.audio = $AudioManager
+	return
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
