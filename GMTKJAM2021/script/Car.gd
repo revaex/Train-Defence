2b809a6de61ca6bbe052cc74a2e83c357b067c19
revaex <thomaskulesz1@gmail.com>
2021-06-12 19:39:30 +1000

    started car movement code (unfinished)

--- GMTKJAM2021/script/Car.gd
+++ GMTKJAM2021/script/Car.gd
@@ -1,5 +1,32 @@
-extends Node2D
+extends KinematicBody2D
+
+# Train reference so the car can know where the carriage connectors are
+onready var train_reference = load("res://scene/entities/Train.tscn").instance()
+var speed = 180
+var friction = 0.2
+var acceleration = 0.1
+var vertical_Variation = 300 # How much the car can move vertically
+onready var horizontal_variation = train_reference.get_node("Carriage1/Area2D/CollisionShape2D").shape.extents.x * 2
+
+var velocity = Vector2.ZERO
 
 
 func _ready():
-	pass
+	print(horizontal_variation)
+	randomize()
+
+func get_movement():
+	var movement = Vector2()
+
+	return movement
+	
+
+func _physics_process(delta):
+	var direction = get_movement()
+	if direction.length() > 0:
+		$AnimatedSprite.play("walking")
+		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, friction)
+		$AnimatedSprite.stop()
+	velocity = move_and_slide(velocity)
--- GMTKJAM2021/script/Projectile.gd
+++ GMTKJAM2021/script/Projectile.gd
@@ -15,6 +15,12 @@ func _physics_process(delta):
 
 func _on_Projectile_body_entered(body):
 	if body.is_in_group("enemies"):
-		print("Dealing " + str(damage) + " damage.")
+		print("Dealing " + str(damage) + " damage to enemy.")
 		body.queue_free()
+	if body._is_in_group("connectors"):
+		print("Dealing " + str(damage) + " damage to connector.")
+		body.queue_free()
+	if body.is_in_group("character"):
+		print("Dealing " + str(damage) + " damage to character.")
+
 	queue_free()
