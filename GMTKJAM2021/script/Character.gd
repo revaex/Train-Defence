e4f14f958be439ecbb2eef5e779487ca0f75def0
revaex <thomaskulesz1@gmail.com>
2021-06-12 16:11:26 +1000

    Added Train/carriage scene and character movement

---
+++ GMTKJAM2021/script/Character.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+var speed = 180
+var friction = 0.2
+var acceleration = 0.1
+var velocity = Vector2.ZERO
+
+
+func _ready():
+	pass
+
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('Up'):
+		input.y -= 1
+	if Input.is_action_pressed('Down'):
+		input.y += 1
+	if Input.is_action_pressed("Left"):
+		input.x -= 1
+	if Input.is_action_pressed('Right'):
+		input.x += 1
+	return input
+
+
+func _physics_process(delta):
+	look_at(get_global_mouse_position())
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, friction)
+	velocity = move_and_slide(velocity)
+
