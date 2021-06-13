11cf7d2eb8e231362886c92ab0b124a4b103276b
Liam <liam.s.oconnell@gmail.com>
2021-06-13 13:39:37 +1000

    More weapons

---
+++ GMTKJAM2021/script/MachineGun.gd
@@ -0,0 +1,7 @@
+extends "res://script/Gun.gd"
+
+func _init():
+	self.damage = 2
+	self.reload_time = 0.2
+	self.projectile = "res://scene/entities/Rocket.tscn"
+	return
---
+++ GMTKJAM2021/script/RocketLauncher.gd
@@ -0,0 +1,7 @@
+extends "res://script/Gun.gd"
+
+func _init():
+	self.damage = 10
+	self.reload_time = 4
+	self.projectile = "res://scene/entities/Rocket.tscn"
+	return
