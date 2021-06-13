abd2bf06e26419c544b9016e2adcfb118913396a
Liam <liam.s.oconnell@gmail.com>
2021-06-13 12:49:49 +1000

    Random environment objects

--- GMTKJAM2021/script/Environment.gd
+++ GMTKJAM2021/script/Environment.gd
@@ -2,7 +2,16 @@ extends Node2D
 
 export var scroll_speed : float
 
+var embellishment = preload("res://scene/environment/Embellishment.tscn")
+
 func _ready():
 	$Dirt.material.set_shader_param("speed_scale", scroll_speed)
 	$Track.material.set_shader_param("speed_scale", scroll_speed*3)
+	return
+
+func _on_SpawnEmbellishment_timeout():
+	var new_decoration = embellishment.instance()
+	new_decoration.position.x = 3460
+	add_child(new_decoration)
+	$SpawnEmbellishment.wait_time = rand_range(1.0, 4.0)
 	return
